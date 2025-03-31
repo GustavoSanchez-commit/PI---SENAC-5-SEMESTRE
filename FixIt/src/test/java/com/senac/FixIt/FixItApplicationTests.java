d74a686cc444c413f16f0ffbf0fac3ab18f72322
Gustavo Sanchez <127356014+GustavoSanchez-commit@users.noreply.github.com>
2025-03-31 13:55:53 -0300

    init Spring Initializr

---
+++ FixIt/src/test/java/com/senac/FixIt/FixItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.FixIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
